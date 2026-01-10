7f4f1a435126058df63a0c6b574e4f35b131eecc
raihan-sifat <sifatraihan2008@gmail.com>
2026-01-11 00:06:26 +0800

    Add burger form created
    Admin panel's file structure has been set up
    Add burger page created

---
+++ base/admin_ui/burger_data.js
@@ -0,0 +1,64 @@
+// defining burger model
+const Burger = sequelize.define(
+    "Burger",
+    {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            set(value) {
+                this.setDataValue("title", value.toLowerCase().trim());
+            },
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        category: {
+            type: DataTypes.ENUM("beef", "chicken", "fish", "vegetable"),
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+            validate: {
+                isDecimal: true,
+                min: 0,
+            },
+            get() {
+                const value = this.getDataValue("price");
+                return value ? parseFloat(value) : 0;
+            },
+        },
+        ingredients: {
+            type: DataTypes.JSON,
+            defaultValue: [],
+            allowNull: false,
+        },
+        coverImage: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: "demo_burger.jpg",
+            field: "cover_image",
+        },
+        images: {
+            type: DataTypes.JSON,
+            defaultValue: [],
+            allowNull: false,
+        },
+        tags: {
+            type: DataTypes.JSON,
+            defaultValue: [],
+            allowNull: false,
+        },
+    },
+    {
+        tableName: "burgers",
+        indexes: [{ fields: ["category"] }, { fields: ["title"] }],
+    }
+);
