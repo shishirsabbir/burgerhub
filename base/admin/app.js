6ab703924f6b6294802fdd8ea388ee8776a8a54b
shishirsabbir <shishir.sabbir@gmail.com>
2026-01-11 08:10:05 +0800

    Added CORS for Development
    Cors allowed for all connection because of the development phase

--- base/admin/app.js
+++ base/admin/app.js
@@ -2,6 +2,7 @@
 import express from "express";
 import morgan from "morgan";
 import burgerRouter from "./routers/burgerRouter.js";
+import cors from "cors";
 
 // express app
 const app = express();
@@ -10,6 +11,15 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
+
+// allowing all connection for development
+app.use(cors());
+
+// app.use(cors({
+//     origin: 'http://localhost:3000', // Your Next.js dev server
+//     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
+//     credentials: true // Required if you decide to use cookies/sessions later
+// }));
 
 app.use(express.json()); // for json data
 app.use(express.urlencoded({ extended: true, limit: "10kb" })); // for form data
