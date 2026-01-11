997f2a6a8e9d5745ab3012c5b16f6ff6a2eb0c0e
raihan-sifat <sifatraihan2008@gmail.com>
2026-01-11 21:37:45 +0800

    Burger List page created
    Burger list page created and designed
    Burgers data are fetch from the api endpoint

--- base/admin_ui/src/pages/listBurger/listBurger.js
+++ base/admin_ui/src/pages/listBurger/listBurger.js
@@ -1,68 +1,60 @@
-// query selection
-const burgerTile = document.getElementById("burger_tile");
-const burgerContainer = document.querySelector(".burger_container");
+//queary the DOM
+const tablebody = document.querySelector(".burger-table__body");
 
-// burger data elements selection
-const burgerTitle = burgerTile.querySelector("h4.burger_title");
-const burgerDescription = burgerTile.querySelector("p.burger_description");
-const burgerPrice = burgerTile.querySelector("p.burger_price");
-// const burgerTagList = burgerTile.querySelector("ul.burger_tag_list");
-
-const getAllBurgers = async (callback) => {
+//getting all burgers
+const getAllBurgers = async () => {
     try {
         const res = await fetch("http://127.0.0.1:8000/api/v1/burgers");
+        // console.log(res);
         const data = await res.json();
-        console.log(data);
-
-        // pass single data
-        callback(data?.data?.burgers[0]);
+        console.log(data.data.burgers[0].images[0]);
+        return data.data.burgers;
     } catch (err) {
-        console.log("Error! ❌");
-        console.error(err);
+        console.log("There is a error");
+        console.log(err);
     }
 };
 
-const createImage = (imgURL, altText, className) => {
-    const img = document.createElement("img");
-
-    img.src = `http://127.0.0.1:8000/img/burgers/${imgURL}`;
-    img.alt = altText;
-
-    // img.onerror = () => {
-    //     img.src = "/dummy_image.jpg";
-    // };
-
-    img.classList.add(className);
-
-    return img;
+// getAllBurgers();
+const updateUI = async () => {
+    const allBurgers = await getAllBurgers();
+    allBurgers.forEach((burger) => {
+        tablebody.innerHTML = `
+        <tr class="burger-table__row">
+            <td
+                class="burger-table__cell burger-table__cell--info">
+                    <div class="burger-info">
+                        <img
+                            class="burger-info__img"
+                            src="http://127.0.0.1:8000/img/burgers/${burger.images[0]}"
+                            alt="Burger"
+                        />
+                        <span class="burger-info__name"
+                            >${burger.title}</span
+                        >
+                    </div>
+                </td>
+                <td class="burger-table__cell">
+                    <span>${burger.category}</span>
+                </td>
+                <td class="burger-table__cell">
+                    <span>${burger.ingredients}</span>
+                </td>
+                <td class="burger-table__cell">
+                    <span>$${burger.price}</span>
+                </td>
+                <td class="burger-table__cell">
+                    <span
+                        ><button
+                            class="btn btn--details"
+                        >
+                            Details
+                        </button></span
+                    >
+                </td>
+            </tr>
+        `;
+    });
 };
 
-const updateBurgerList = (data) => {
-    burgerTitle.textContent = data.title;
-    burgerDescription.textContent = data.description;
-    burgerPrice.textContent = data.price;
-
-    const burgerCoverImage = createImage(
-        data.coverImage,
-        `${data.title} Cover Image`,
-        "burger_cover"
-    );
-
-    burgerTile.appendChild(burgerCoverImage);
-
-    const imgArray = data.images.map((imgFile, i) => {
-        const imgTag = createImage(
-            imgFile,
-            `${data.title} Image ${i}`,
-            "burger_image"
-        );
-        return imgTag;
-    });
-
-    imgArray.forEach((imgTag) => {
-        burgerTile.appendChild(imgTag);
-    });
-};
-
-// running the fetch function
-getAllBurgers(updateBurgerList);
+updateUI();
